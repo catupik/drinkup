b348dbb16aa7405c10dce02c9589e82ffb06376d
catupik <gnedyshevaeg@gmail.com>
2023-10-25 22:48:05 +0200

    second commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import videodrink from "./videodrink.mp4";
+import CoctailComponents from "./CoctailComponents";
 
 function App() {
+  const [mySearch, setMySearch] = useState("");
+  const [wordSubmitted, setWordSubmitted] = useState();
+  const [myCoctail, setMyCoctail] = useState([]);
+
+  useEffect(() => {
+    const getCoctail = async () => {
+      try{
+        const response = await fetch(
+          `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${wordSubmitted}`
+        );
+        if(!response.ok){
+          throw new Error(`HTTP error! Status: ${response.status}`)
+        }
+        const data = await response.json();
+
+        if(data.drinks === null){
+          console.error('data.drinks is null')
+          setMyCoctail([])
+        } else{
+          setMyCoctail(data.drinks);
+        
+        }
+
+      } catch (error){
+        console.error("Error fetching cocktail data:", error);
+        setMyCoctail([]);
+      }   
+    };
+    getCoctail();
+  }, [wordSubmitted]);
+
+  const myCoctailSearch = (e) => {
+    setMySearch(e.target.value);
+  };
+
+  const finalSearch = (e) => {
+    e.preventDefault();
+    setWordSubmitted(mySearch);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <video autoPlay muted loop>
+          <source src={videodrink} type="video/mp4" />
+        </video>
+        <h1>Drink up!</h1>
+      </div>
+
+      <div className="container">
+        <form onSubmit={finalSearch}>
+          <input
+            className="container"
+            onChange={myCoctailSearch}
+            value={mySearch}
+          ></input>
+        </form>
+      </div>
+
+      <div className="container">
+        <button onClick={finalSearch}
+        >find your coctail</button>
+      </div>
+
+    <div className="coctails">
+    {myCoctail.map((element, index) =>{
+      
+      const ingredients = Array.from({length:15},(_, i)=> element[`strIngredient${i + 1}`]).filter(Boolean);
+
+      return(
+      
+      <CoctailComponents key={index}
+        name={element.strDrink}
+        image={element.strDrinkThumb}
+        instructions={element.strInstructions}
+        ingredients={ingredients}
+      />
+    );})}
+      </div>
     </div>
   );
 }
---
+++ src/CoctailComponents.js
@@ -0,0 +1,27 @@
+function CoctailComponents({ name, image, instructions, ingredients }) {
+  return (
+    <div className="bord">
+      <div className="container">
+        <h2>{name}</h2>
+      </div>
+
+      <div className="container">
+        <img src={image} alt="coctail" width="200" />
+      </div>
+      <div className="container">
+        <ul>
+            {ingredients.map((element, index)=>(
+                <li key={index}>{element}</li>
+            ))}
+        </ul>
+        
+      </div>
+
+      <div className="container description">
+        <h4>{instructions}</h4>
+      </div>
+    </div>
+  );
+}
+
+export default CoctailComponents;
